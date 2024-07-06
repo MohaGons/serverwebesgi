725e174f20e2c0c27c978a53dbd5492e7ac2c248
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-07-06 14:22:10 +0200

    modification du projet docker

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.get("/products", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5001/products")
+    fetch("http://localhost:3001/products")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
